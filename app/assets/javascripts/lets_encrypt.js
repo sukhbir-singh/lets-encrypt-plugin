6e4330b569e1b19294311ce97a4b6cdb628d6af7
sukhbir-singh <sukhbir947@gmail.com>
2019-08-19 02:34:54 +0530

    added ajax calls

--- app/assets/javascripts/lets_encrypt.js
+++ app/assets/javascripts/lets_encrypt.js
@@ -1,8 +1,11 @@
-$(document).on('click', '#refresh-btn', function(event) {
+
+var BASE_URL = "http://localhost:4400/certi/";
+
+$("#refresh-btn").click(function(){
   showSpinner();
 });
 
-$(document).on('click', '#configure-btn', function(event) {
+$("#configure-btn").click(function(){
   hideSpinner();
 });
 
@@ -15,3 +18,44 @@ function hideSpinner(){
   var spinner=$("#main-spinner")[0];
   spinner.style.zIndex="-1"
 }
+
+$("#generate-btn").click(function(){
+  showSpinner();
+  $.ajax({
+    method: "POST",
+    dataType: "json",
+    headers: {
+    },
+    url: BASE_URL + "generate",
+    success: function(result){
+      console.log(result);
+      hideSpinner();
+    }
+  });
+});
+
+$("#renew-btn").click(function(){
+  showSpinner();
+  $.ajax({
+    method: "POST",
+    dataType: "json",
+    url: BASE_URL + "renew",
+    success: function(result){
+      console.log(result);
+      hideSpinner();
+    }
+  });
+});
+
+$("#delete-btn").click(function(){
+  showSpinner();
+  $.ajax({
+    method: "POST",
+    dataType: "json",
+    url: BASE_URL + "delete",
+    success: function(result){
+      console.log(result);
+      hideSpinner();
+    }
+  });
+});
